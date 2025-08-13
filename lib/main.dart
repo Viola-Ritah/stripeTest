7433c05b19fbcc8c76027130d93beddf73d485b4
Viola-Ritah <wakooliviola@gmail.com>
2025-08-13 21:31:36 +0300

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: ,
+      home: const Homepage(),
     );
   }
 }
